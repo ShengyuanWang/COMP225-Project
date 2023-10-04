2bddd35e4390295c824cfe33ddc060514484af8b
Shengyuan Wang <78770936+ShengyuanWang@users.noreply.github.com>
2023-10-04 15:51:10 -0500

    bug-solve: bg-color problem

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="menu">
+  <div class="menu bg_c">
       <div @click="isCollapse = !isCollapse" class="menu_icon"></div>
     <el-menu
         default-active="2"
-        class="el-menu-vertical-demo"
+        class="el-menu-vertical-demo bg_c"
         :collapse="isCollapse"
         @open="handleOpen"
         @close="handleClose"
@@ -14,31 +14,17 @@
           <el-icon ><location /></el-icon>
           <span>Navigator One</span>
         </template>
-        <el-menu-item-group>
+        <el-menu-item-group class="bg_c">
           <template #title><span>Group One</span></template>
           <el-menu-item index="1-1">item one</el-menu-item>
           <el-menu-item index="1-2">item two</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
+        <el-menu-item-group class="bg_c">
+          <template #title><span>Group Two</span></template>
+          <el-menu-item index="2-1">item one</el-menu-item>
+          <el-menu-item index="2-2">item two</el-menu-item>
         </el-menu-item-group>
-        <el-sub-menu index="1-4">
-          <template #title><span>item four</span></template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-sub-menu>
       </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon><icon-menu /></el-icon>
-        <template #title>Navigator Two</template>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <el-icon><document /></el-icon>
-        <template #title>Navigator Three</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon><setting /></el-icon>
-        <template #title>Navigator Four</template>
-      </el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -90,5 +76,8 @@ const handleClose = (key: string, keyPath: string[]) => {
   margin-top: 10px;
 }
 
+.bg_c {
+  background-color: #C3BCB3;
+}
 
 </style>
