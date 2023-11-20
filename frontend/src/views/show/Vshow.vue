591789b1b4486e04b84063b059dd8e379a0eeda2
Shengyuan Wang <78770936+ShengyuanWang@users.noreply.github.com>
2023-11-20 13:36:28 -0600

    fix temporary vshow error

--- frontend/src/views/show/Vshow.vue
+++ frontend/src/views/show/Vshow.vue
@@ -55,7 +55,7 @@ onBeforeMount(()=>{
   <div class="drink-table" >
     <drink-item v-for="drink in drinks"
                 :name="drink.name"
-                :instruction="drink.instructions"
+                :instruction="drink.ingredients"
                 :description="drink.description"
                 url="https://firebasestorage.googleapis.com/v0/b/comp225-810dc.appspot.com/o/images%2FredWine.png?alt=media&token=2af7d5ed-b1e1-4382-95fe-2cdcc62fa79e"
                 @click="getData(drink)"
