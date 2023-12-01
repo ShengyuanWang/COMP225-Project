d828d580dd5bc2d44c53cf6c4d4d56801df190d3
Cora <38634654+afortiori@users.noreply.github.com>
2023-12-01 14:07:33 -0600

    synonyms working

--- backend/api/scripts.py
+++ backend/api/scripts.py
@@ -1,6 +1,6 @@
 import json
 
-def collect_genres():
+def collect_all_genres():
     """" Extracts genres and makes json with all of them"""
 
     with open('book-alcohol-pairings.json') as input_file:
@@ -8,11 +8,19 @@ def collect_genres():
 
     genre_list = []
     for x in data["alcohols"]:
-        genre_list+=x["genres"]
-        print(x)
-    newList =list(set(genre_list))
+        genre_list+= x["genres"] + x["key genres"]
+    newList = list(set(genre_list))
     
     with open('output.json', 'w') as output_file:
         json.dump(newList, output_file, indent=4)
 
-collect_genres()
+def transform_json(input_file):
+    with open(input_file, 'r') as f:
+        original_json = json.load(f)
+
+    transformed_data = {value: key for key, values in original_json.items() for value in values}
+
+    with open("synonyms_lookup.json", 'w') as f:
+        json.dump(transformed_data, f, indent=2)
+
+transform_json("synonyms.json")
