412dddf423ac9d7bfd448a7173f28e3a3890f672
Shengyuan Wang <78770936+ShengyuanWang@users.noreply.github.com>
2023-12-08 14:21:02 -0600

    fix place bug

--- frontend/src/views/match/Vmatch.vue
+++ frontend/src/views/match/Vmatch.vue
@@ -21,7 +21,54 @@ const title = route.query.title;
 const author = route.query.author;
 const show = () => {
   showResult.value = true
+}
 
+const allergies = ref({gluten: false,
+  lactose: false,
+  egg: false,
+  treenuts: false,
+  soy: false,
+  shellfish: false,
+  fish: false});
+const types = ref({  Beer: true,
+  Wine: true,
+  Spirits: true,
+  Cocktails: true});
+// 父组件传递过来事件
+
+const findTypes = ref('')
+const findAllergies = ref('')
+
+const childValFn = (e)=>{
+  //接收子组件传递给父组件的值
+  console.log(e)
+  allergies.value = e.allergy.value;
+  types.value = e.preference.value;
+  let allergyList = [];
+  for (const allergy in allergies.value) {
+    if (allergies.value[allergy] === true) {
+      allergyList.push(allergy)
+    }
+  }
+  if (allergyList.length > 0) {
+    findAllergies.value = allergyList.join(',')
+  } else {
+    findAllergies.value = 'Nan'
+  }
+
+  let typeList = [];
+  for (const type in types.value) {
+    if (types.value[type] === true) {
+      typeList.push(type)
+    }
+  }
+  if (typeList.length > 0) {
+    findTypes.value = typeList.join(',')
+  } else {
+    findTypes.value = 'Nan'
+  }
+
+  console.log(findTypes, findAllergies)
 }
 
 onMounted(()=>{
@@ -39,9 +86,9 @@ onMounted(()=>{
 
 
   <div class="fullscreen">
-    <Menu></Menu>
+    <Menu @childClick="childValFn"></Menu>
     <div class="header">
-      <Qinput place="40" :place-holder="placeHolder" :input="input"></Qinput>
+      <Qinput move place="60" place-holder="Please type in the book name" :find-allergies=findAllergies :find-types=findTypes></Qinput>
     </div>
     <div class="loading" v-show="!showResult">
       <h1 style="padding-left: 20vw; padding-top: 20vh; font-size: 40px">Loading Result ....</h1>
